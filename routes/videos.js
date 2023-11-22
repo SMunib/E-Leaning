0abe8c0d1e03d6f8e719cd41c8d9265fc0762b4a
SMunib <munibrehman10@gmail.com>
2023-11-23 04:36:53 +0500

    all controllers created

--- routes/videos.js
+++ routes/videos.js
@@ -1,118 +1,20 @@
 const express = require('express');
 const router = express.Router();
 const mydb = require('./db');
+const videoController = require('../controllers/videos');
 
 router
   .route('/add')
-  .post(async(req,res)=>{
-    const {CourseID,URL,Completed} = req.body;
-    const query = "Insert into videos (CourseID,URL,Completed) values (?,?,?)";
-    try{
-      mydb.query(query,[CourseID,URL,Completed],(err) => {
-        if(err){
-          console.log('Error: Failed to insert into database: '+err);
-          return res.status(400).json({message:err.message});
-        }
-        res.status(201).json({
-          status:'ok',
-          message:'New Video Added',
-        });
-      });
-    }catch(err){
-      console.log('Error::'+err);
-      res.status(400).send();
-    }
-  });
+  .post(videoController.addVideo);
+  
 router
   .route('/find')
-  .get(async(req,res) => {
-    const query = 'Select * from videos';
-    try{
-      mydb.query(query,(err,results) => {
-        if(err){
-          console.log('Error Reading from Database' +err);
-          return res.status(400).json({message:err.message});
-        }
-        res.status(201).json({
-          status:'ok',
-          message:'retrieved',
-          data:results,
-        });
-      });
-    }catch(err){
-      console.log('Error:: '+err);
-      res.status(500).send();
-    }
-  });
+  .get(videoController.findVideos);
+
 router
   .route('/find/:CourseID')
-  .get(async(req,res) => {
-    var id = req.params.CourseID;
-    const query = 'Select * from videos where CourseID = ?';
-    try{
-      mydb.query(query,[id],(err,results) => {
-        if(err){
-          console.log('Failed to execute query: '+err);
-          return res.status(400).json({message:err.message});
-        }
-        if(!results){
-          console.log('Video does not exist ');
-          return res.status(404).json({message:'Error: Video Does not Exist in the Table'});
-        }
-        res.status(201).json({
-          status:'ok',
-          message:'data found',
-          data:results,
-        });
-      });
-    }catch(err){
-      console.log('Error::'+err);
-      res.status(500).send();
-    }
-  })
-  .delete(async(req,res)=>{
-    var id = req.params.CourseID;
-    var url = req.params.URL;
-    const query = 'Delete from videos where CourseID = ?';
-    try{
-      mydb.query(query,[id,url],(err,results) => {
-        if(err) return res.status(400).json({message:err.message});
-        if(results.affectedRows === 0) return res.status(404).json({message:'Video not Found'});
-        return res.status(201).json({
-          status:"ok",
-          message:"Tuple Deleted",
-        });
-      });
-    }catch(err){
-      console.log('Error:: '+err);
-      res.status(500).send();
-    }
-  })
-  .patch(async(req,res) => {
-    const allowedAttributes = ['Completed','URL'];
-    const id = req.params.CourseID;
-    const url = req.params.URL;
-    const updateAttributes = Object.keys(req.body)
-      .filter(attr => allowedAttributes.includes(attr))
-      .map(attr => `${attr}=?`)//backticks
-      .join(', ');
-
-    if(!updateAttributes) return res.status(400).json({message: 'No Valid attributes provided for update'});
-    const query = `update videos set ${updateAttributes} where CourseID = ?`;//backticks
-    const values = [...Object.values(req.body).filter((_,index) => allowedAttributes.includes(Object.keys(req.body)[index])),id,url];
-    try{
-        mydb.query(query,values,(err,results)=>{
-        if (err) return res.status(400).json({message:err.message});
-        if(results.affectedRows === 0) return res.status(404).json({message:'Video not Found in Table'});
-        res.status(201).json({
-          status:"ok",
-          message:"Updated",
-          data:results,
-        });
-      });
-    }catch(err){
-      console.log('Error::' +err);res.status(500).send();
-    }
-  });
+  .get(videoController.findSpecificVideo)
+  .delete(videoController.removeVideo)
+  .patch(videoController.modifyVideo);
   
 module.exports = router;
