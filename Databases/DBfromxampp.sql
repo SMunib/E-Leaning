5ff920fe8f78aff089c2610b7e892ce1f0be7a72
SMunib <munibrehman10@gmail.com>
2023-11-30 09:27:38 +0500

    updated database

--- Databases/DBfromxampp.sql
+++ Databases/DBfromxampp.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 27, 2023 at 01:18 AM
+-- Generation Time: Nov 30, 2023 at 05:26 AM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -14,6 +14,8 @@ SET time_zone = "+00:00";
 --
 -- Database: `mydb`
 --
+CREATE DATABASE IF NOT EXISTS `mydb` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `mydb`;
 
 -- --------------------------------------------------------
 
@@ -23,9 +25,8 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `admin` (
   `ID` int(11) NOT NULL,
-  `Name` varchar(100) NOT NULL,
-  `Email` varchar(100) NOT NULL,
-  `Password` varchar(255) NOT NULL
+  `Requests` varchar(255) DEFAULT NULL,
+  `Responses` varchar(255) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -35,13 +36,10 @@ CREATE TABLE `admin` (
 --
 
 CREATE TABLE `course` (
-  `CourseID` varchar(255) NOT NULL,
-  `CourseName` varchar(45) NOT NULL,
-  `Duration` int(11) DEFAULT NULL,
-  `TeacherID` int(255) NOT NULL,
-  `Modules` int(11) NOT NULL,
-  `StudentID` int(45) NOT NULL,
-  `Grade` int(11) DEFAULT NULL
+  `CourseID` int(11) NOT NULL,
+  `CourseName` varchar(255) NOT NULL,
+  `Modules` int(11) DEFAULT NULL,
+  `Duration` time NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -56,6 +54,19 @@ CREATE TABLE `discussionforum` (
   `Comment` text DEFAULT NULL,
   `Timestamp` datetime(6) DEFAULT NULL,
   `StudentID` int(45) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reg_course`
+--
+
+CREATE TABLE `reg_course` (
+  `CourseID` varchar(255) NOT NULL,
+  `TeacherID` int(255) NOT NULL,
+  `StudentID` int(45) NOT NULL,
+  `Grade` int(11) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 
 -- --------------------------------------------------------
@@ -81,16 +92,9 @@ CREATE TABLE `student` (
 --
 
 INSERT INTO `student` (`StudentID`, `FirstName`, `LastName`, `Email`, `Password`, `UniversityName`, `Country`, `City`, `PostalCode`) VALUES
-(1, NULL, NULL, 'munibrehman10@gmail.com', '$2a$10$qjqft8UA03YXI4fbVnHAR.N6fRvQaJeAmk3So4d5BftfCrg1A7Hc2', NULL, NULL, NULL, NULL),
-(14, NULL, NULL, 'dummy2@gmail.com', '$2a$10$G0C5jd5suGAsKe15tNOWZebTI7/N5Rfr5OxJDg3vptLt1nLlBOC4a', NULL, NULL, NULL, NULL),
-(15, NULL, NULL, 'dummy3@gmail.com', '$2a$10$N4M3cL9NnUlRBw7k1ARijuAIoKJO2yCWh4H0qLQ2J1A7CaiBiHHXO', NULL, NULL, NULL, NULL),
-(16, NULL, NULL, 'dummy212@gmail.com', '$2a$10$dLQBLvXPG2SUNTFa1Z6JpuDfZ009SC/i7LE1wnfFbZPw1W69jGlki', NULL, NULL, NULL, NULL),
-(17, NULL, NULL, 'dummy23@gmail.com', '$2a$10$wxkUa7p/bGniai64TYcOUOa4SZRrjwY.jXYhqqZmHkJlEi0tc0GCa', NULL, NULL, NULL, NULL),
-(18, NULL, NULL, 'dummy7@gmail.com', '$2a$10$ERqWrsQPYHnoOX7EJe2IJetZrhMB8hSkTGWn.8Z.B8K61T4GK/vTe', NULL, NULL, NULL, NULL),
-(19, NULL, NULL, 'd@g.c', '$2a$10$fNRtYfUJqXYR9iMXKfN1FOI.KXXTExBfzADRZ5/cUqnFB3vKo4TJ6', NULL, NULL, NULL, NULL),
-(20, NULL, NULL, 'dummy9@gmail.com', '$2a$10$RekLf5EtQjYtrFsm9imOoOnKOhN4P2YXkwN.EfhXLpM9ZEYHNROwC', NULL, NULL, NULL, NULL),
-(21, NULL, NULL, 'a@b.c', '$2a$10$vBGWB6wAlbGkMUjtwvq.JOxMUGmt5lbPkSdDzcUi3IAphfJuc5h8.', NULL, NULL, NULL, NULL),
-(22, 'a', 'a', 'a@b.k', '$2a$10$2irSHMLJWEkvfF/9X6b/BuNgsUsOGQ0xLAmlkltkjRj4AEpLBP.rm', 'a', 'a', 'a', '123');
+(23, 'Munib', 'Rehman', 'munibrehman10@gmail.com', '$2a$10$LHTfujE1AgBi94Rr5yacu..EWsXIU.qbwyCmTPkRlKDgOUzMWuDVq', 'Fast', 'Pakistan', 'Karachi', '12345'),
+(24, 'munib', 'rehman', 'munibrehman15@gmail.com', '$2a$10$JZkfNpiqQz8R0ph/OW.RP.ZP56H9okLJi6PE7twpGlMF6sYkdqpm6', 'fast', 'Canada', 'lahore', '12345'),
+(25, 'Munib', 'Rehman', 'munibrehman1920@gmail.com', '$2a$10$uEXO1vHIeIxg7TwIDF60DuxNqhwfZSUGspHikN6PQK9ufbrzgOzXW', 'NUCES', 'Pakistan', 'Karachi', '12543');
 
 -- --------------------------------------------------------
 
@@ -110,6 +114,13 @@ CREATE TABLE `teacher` (
   `PostalCode` varchar(45) DEFAULT NULL,
   `AccountNo` varchar(45) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
+
+--
+-- Dumping data for table `teacher`
+--
+
+INSERT INTO `teacher` (`TeacherID`, `FirstName`, `LastName`, `Email`, `Password`, `Qualification`, `City`, `Country`, `PostalCode`, `AccountNo`) VALUES
+(7, 'Munib', 'Rehman', 'munibrehman1920@gmail.com', '$2a$10$XnklM2GTx8Tj5bESDMaCQus8f76oj1M0xTC83o', NULL, 'Karachi', 'Pakistan', '12345', '123456789');
 
 -- --------------------------------------------------------
 
@@ -138,10 +149,8 @@ ALTER TABLE `admin`
 -- Indexes for table `course`
 --
 ALTER TABLE `course`
-  ADD PRIMARY KEY (`CourseID`,`TeacherID`,`StudentID`) USING BTREE,
-  ADD KEY `StudentID_idx` (`StudentID`),
-  ADD KEY `InstructorID_idx` (`TeacherID`),
-  ADD KEY `CourseID_idx` (`CourseID`) USING BTREE;
+  ADD PRIMARY KEY (`CourseID`),
+  ADD UNIQUE KEY `CourseName_idx` (`CourseName`);
 
 --
 -- Indexes for table `discussionforum`
@@ -149,6 +158,15 @@ ALTER TABLE `course`
 ALTER TABLE `discussionforum`
   ADD PRIMARY KEY (`CourseID`,`StudentID`),
   ADD KEY `StudentID_idx` (`StudentID`);
+
+--
+-- Indexes for table `reg_course`
+--
+ALTER TABLE `reg_course`
+  ADD PRIMARY KEY (`CourseID`,`TeacherID`,`StudentID`) USING BTREE,
+  ADD KEY `StudentID_idx` (`StudentID`),
+  ADD KEY `InstructorID_idx` (`TeacherID`),
+  ADD KEY `CourseID_idx` (`CourseID`) USING BTREE;
 
 --
 -- Indexes for table `student`
@@ -188,35 +206,35 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
-  MODIFY `StudentID` int(45) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
+  MODIFY `StudentID` int(45) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
 
 --
 -- AUTO_INCREMENT for table `teacher`
 --
 ALTER TABLE `teacher`
-  MODIFY `TeacherID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+  MODIFY `TeacherID` int(255) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `course`
---
-ALTER TABLE `course`
-  ADD CONSTRAINT `FK_StudentID` FOREIGN KEY (`StudentID`) REFERENCES `student` (`StudentID`),
-  ADD CONSTRAINT `FK_TeacherID` FOREIGN KEY (`TeacherID`) REFERENCES `teacher` (`TeacherID`);
-
---
 -- Constraints for table `discussionforum`
 --
 ALTER TABLE `discussionforum`
   ADD CONSTRAINT `FK_Student` FOREIGN KEY (`StudentID`) REFERENCES `student` (`StudentID`),
-  ADD CONSTRAINT `discussionforum_ibfk_1` FOREIGN KEY (`CourseID`) REFERENCES `course` (`CourseID`) ON DELETE NO ACTION;
+  ADD CONSTRAINT `discussionforum_ibfk_1` FOREIGN KEY (`CourseID`) REFERENCES `reg_course` (`CourseID`) ON DELETE NO ACTION;
+
+--
+-- Constraints for table `reg_course`
+--
+ALTER TABLE `reg_course`
+  ADD CONSTRAINT `FK_StudentID` FOREIGN KEY (`StudentID`) REFERENCES `student` (`StudentID`),
+  ADD CONSTRAINT `FK_TeacherID` FOREIGN KEY (`TeacherID`) REFERENCES `teacher` (`TeacherID`);
 
 --
 -- Constraints for table `videos`
 --
 ALTER TABLE `videos`
-  ADD CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`CourseID`) REFERENCES `course` (`CourseID`) ON DELETE NO ACTION;
+  ADD CONSTRAINT `videos_ibfk_1` FOREIGN KEY (`CourseID`) REFERENCES `reg_course` (`CourseID`) ON DELETE NO ACTION;
 COMMIT;
